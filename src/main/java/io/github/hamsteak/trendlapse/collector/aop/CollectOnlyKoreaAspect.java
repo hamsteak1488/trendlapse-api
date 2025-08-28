01357be9864daa1f6acce0a4f72ca99035ccfd90
신재민 <woals1488@naver.com>
2025-08-28 17:12:02 +0900

    refactor: 한국 지역 Trending만 수집하도록 처리하는 특수 로직을 AOP 로 분리함으로써 추후 해당 특수 로직에 변경이 있더라도 핵심 로직에는 수정이 없도록 함.

---
+++ src/main/java/io/github/hamsteak/trendlapse/collector/aop/CollectOnlyKoreaAspect.java
@@ -0,0 +1,22 @@
+package io.github.hamsteak.trendlapse.collector.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Aspect
+@Component
+@ConditionalOnProperty(name = "only-korea-region", havingValue = "true")
+public class CollectOnlyKoreaAspect {
+    @Around("execution(* io.github.hamsteak.trendlapse.collector.domain.TrendingCollector.collect(..))")
+    public Object logApiCallTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        joinPoint.getArgs()[2] = List.of("KR");
+        return joinPoint.proceed();
+    }
+}
--- src/main/java/io/github/hamsteak/trendlapse/collector/domain/TrendingCollectScheduler.java
+++ src/main/java/io/github/hamsteak/trendlapse/collector/domain/TrendingCollectScheduler.java
@@ -5,7 +5,6 @@ import io.github.hamsteak.trendlapse.region.domain.RegionReader;
 import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Clock;
@@ -20,9 +19,6 @@ public class TrendingCollectScheduler {
     private final TrendingCollector trendingCollector;
     private final CollectSchedulerProperties collectSchedulerProperties;
 
-    @Value("${only-korea-region:false}")
-    private boolean onlyKoreaRegion;
-
     @Timed("collect.whole")
     //    @Scheduled(cron = "${collect-scheduler.collect-cron}", zone = "UTC")
     public void collect() {
@@ -36,10 +32,6 @@ public class TrendingCollectScheduler {
 
         if (regionCodes.size() < 110) {
             log.warn("Region list contains fewer than 110 items. This may cause incomplete processing.");
-        }
-
-        if (onlyKoreaRegion) {
-            regionCodes = regionCodes.stream().filter(regionCode -> regionCode.equals("KR")).toList();
         }
 
         trendingCollector.collect(dateTime, collectSchedulerProperties.getCollectSize(), regionCodes);
