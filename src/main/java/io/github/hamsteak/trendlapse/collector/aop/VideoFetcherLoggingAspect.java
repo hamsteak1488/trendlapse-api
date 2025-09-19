02aca0f1db93e3491a0c176881d655f0ee0f9986
신재민 <woals1488@naver.com>
2025-09-20 04:15:12 +0900

    feat: Video, Channel 수집 프로세스 세부 단계 소요시간 로깅 기능 추가.

---
+++ src/main/java/io/github/hamsteak/trendlapse/collector/aop/VideoFetcherLoggingAspect.java
@@ -0,0 +1,31 @@
+package io.github.hamsteak.trendlapse.collector.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Slf4j
+@Aspect
+@Component
+@ConditionalOnProperty(prefix = "collector", name = "use-log", havingValue = "true")
+public class VideoFetcherLoggingAspect {
+    @Around("execution(* io.github.hamsteak.trendlapse.collector.fetcher.VideoFetcher.fetch(..))")
+    public Object logFetchTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("Started video fetch task.");
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        Object result = joinPoint.proceed();
+
+        stopWatch.stop();
+
+        log.info("Finished video fetch task. (elapsed {}ms)", stopWatch.lastTaskInfo().getTimeMillis());
+
+        return result;
+    }
+}
