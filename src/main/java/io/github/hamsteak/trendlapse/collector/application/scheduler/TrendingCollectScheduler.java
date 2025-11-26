74a0c55cb4938c36eb9b12f1d07bceb3c84ec128
신재민 <woals1488@naver.com>
2025-11-26 17:11:31 +0900

    refactor: 지역과 인기 급상승 동영상 수집하는 로직을 스케줄러가 직접 수행하지 않고 서비스에게 위임하도록 변경.

--- src/main/java/io/github/hamsteak/trendlapse/collector/application/scheduler/TrendingCollectScheduler.java
+++ src/main/java/io/github/hamsteak/trendlapse/collector/application/scheduler/TrendingCollectScheduler.java
@@ -1,53 +1,24 @@
 package io.github.hamsteak.trendlapse.collector.application.scheduler;
 
-import io.github.hamsteak.trendlapse.collector.application.CollectSchedulerProperties;
-import io.github.hamsteak.trendlapse.collector.application.component.collector.TrendingCollector;
-import io.github.hamsteak.trendlapse.collector.application.component.fetcher.RegionFetcher;
-import io.github.hamsteak.trendlapse.collector.application.component.storer.RegionStorer;
-import io.github.hamsteak.trendlapse.collector.application.dto.RegionItem;
-import io.github.hamsteak.trendlapse.region.domain.Region;
-import io.github.hamsteak.trendlapse.region.infrastructure.RegionRepository;
+import io.github.hamsteak.trendlapse.collector.application.service.TrendingCollectService;
 import io.micrometer.core.annotation.Timed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.Clock;
-import java.time.LocalDateTime;
-import java.util.List;
-
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class TrendingCollectScheduler {
-    private final RegionRepository regionRepository;
-    private final RegionFetcher regionFetcher;
-    private final RegionStorer regionStorer;
-    private final TrendingCollector trendingCollector;
-    private final CollectSchedulerProperties collectSchedulerProperties;
+    private final TrendingCollectService trendingCollectService;
 
     @Timed("collect.whole")
     @Scheduled(cron = "${collect-scheduler.collect-cron}", zone = "UTC")
     public void collect() {
         log.info("Starting scheduled trending collection job.");
 
-        List<String> regionCodes = regionRepository.findAll().stream()
-                .map(Region::getRegionCode)
-                .toList();
-
-        if (regionCodes.size() < regionFetcher.getExternalRegionCount()) {
-            log.info("Starting to fetch regions because the number of regions is fewer than {} items.", regionFetcher.getExternalRegionCount());
-            List<RegionItem> regionItems = regionFetcher.fetch();
-            regionStorer.store(regionItems);
-            regionCodes = regionRepository.findAll().stream()
-                    .map(Region::getRegionCode)
-                    .toList();
-        }
-
-        LocalDateTime now = LocalDateTime.now(Clock.systemUTC());
-
-        trendingCollector.collect(now, collectSchedulerProperties.getCollectSize(), regionCodes);
+        trendingCollectService.collect();
 
         log.info("Completed scheduled trending collection job.");
     }
---
+++ src/main/java/io/github/hamsteak/trendlapse/collector/application/service/TrendingCollectService.java
@@ -0,0 +1,46 @@
+package io.github.hamsteak.trendlapse.collector.application.service;
+
+import io.github.hamsteak.trendlapse.collector.application.CollectSchedulerProperties;
+import io.github.hamsteak.trendlapse.collector.application.component.collector.TrendingCollector;
+import io.github.hamsteak.trendlapse.collector.application.component.fetcher.RegionFetcher;
+import io.github.hamsteak.trendlapse.collector.application.component.storer.RegionStorer;
+import io.github.hamsteak.trendlapse.collector.application.dto.RegionItem;
+import io.github.hamsteak.trendlapse.region.domain.Region;
+import io.github.hamsteak.trendlapse.region.infrastructure.RegionRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TrendingCollectService {
+    private final RegionRepository regionRepository;
+    private final RegionFetcher regionFetcher;
+    private final RegionStorer regionStorer;
+    private final TrendingCollector trendingCollector;
+    private final CollectSchedulerProperties collectSchedulerProperties;
+
+    public void collect() {
+        List<String> regionCodes = regionRepository.findAll().stream()
+                .map(Region::getRegionCode)
+                .toList();
+
+        if (regionCodes.size() < regionFetcher.getExternalRegionCount()) {
+            log.info("Starting to fetch regions because the number of regions is fewer than {} items.", regionFetcher.getExternalRegionCount());
+            List<RegionItem> regionItems = regionFetcher.fetch();
+            regionStorer.store(regionItems);
+            regionCodes = regionRepository.findAll().stream()
+                    .map(Region::getRegionCode)
+                    .toList();
+        }
+
+        LocalDateTime now = LocalDateTime.now(Clock.systemUTC());
+
+        trendingCollector.collect(now, collectSchedulerProperties.getCollectSize(), regionCodes);
+    }
+}
