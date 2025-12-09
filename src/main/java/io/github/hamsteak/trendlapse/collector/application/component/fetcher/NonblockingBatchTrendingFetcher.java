beab7cbd6a1afaadcff22d33986bc5b8ca6bd2b9
신재민 <woals1488@naver.com>
2025-12-09 18:42:30 +0900

    refactor: List 대신 Stream을 사용할 경우 이점이 없고 되려 잘못 사용할 위험만 크기 때문에 Stream을 다시 List로 변경함. 또한 block 메서드 null 반환하는 경우를 대비하는 코드 추가.

--- src/main/java/io/github/hamsteak/trendlapse/collector/application/component/fetcher/NonblockingBatchTrendingFetcher.java
+++ src/main/java/io/github/hamsteak/trendlapse/collector/application/component/fetcher/NonblockingBatchTrendingFetcher.java
@@ -5,6 +5,7 @@ import io.github.hamsteak.trendlapse.youtube.infrastructure.NonblockingYoutubeDa
 import io.github.hamsteak.trendlapse.youtube.infrastructure.dto.TrendingListResponse;
 import io.github.hamsteak.trendlapse.youtube.infrastructure.dto.VideoResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
@@ -16,6 +17,7 @@ import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+@Slf4j
 @Primary
 @Component
 @RequiredArgsConstructor
@@ -24,40 +26,50 @@ public class NonblockingBatchTrendingFetcher implements TrendingFetcher {
 
     @Override
     public List<TrendingItem> fetch(LocalDateTime dateTime, int collectSize, List<String> regionCodes, int maxResultCount) {
-        return Flux.fromIterable(regionCodes)
+        List<TrendingItem> trendingItems = Flux.fromIterable(regionCodes)
                 .flatMap(regionCode ->
                         fetchTrendings(collectSize, regionCode, maxResultCount, null)
                                 .map(videoResponses -> mapFromResponsesToItems(dateTime, regionCode, videoResponses))
-                                .flatMapMany(Flux::fromStream))
+                                .flatMapMany(Flux::fromIterable))
                 .collectList()
                 .block();
+
+        if (trendingItems == null) {
+            log.warn("Trending fetch appears to have failed; list is empty. regionCodes={}", regionCodes);
+            return List.of();
+        }
+
+        return trendingItems;
     }
 
-    private Mono<Stream<VideoResponse>> fetchTrendings(int remainingCount, String regionCode, int maxResultCount, String pageToken) {
+    private Mono<List<VideoResponse>> fetchTrendings(int remainingCount, String regionCode, int maxResultCount, String pageToken) {
         int resultCount = Math.min(remainingCount, maxResultCount);
         Mono<TrendingListResponse> responseMono = nonblockingYoutubeDataApiCaller.fetchTrendings(resultCount, regionCode, pageToken);
 
         return responseMono.flatMap(trendingListResponse -> {
-            Stream<VideoResponse> trendingResponses = trendingListResponse.getItems().stream();
+            List<VideoResponse> trendingResponses = trendingListResponse.getItems();
 
             if (trendingListResponse.getNextPageToken() == null || remainingCount == resultCount) {
                 return Mono.just(trendingResponses);
             }
 
-            Mono<Stream<VideoResponse>> postPageResponseMono = fetchTrendings(remainingCount - resultCount, regionCode, maxResultCount, trendingListResponse.getNextPageToken());
+            Mono<List<VideoResponse>> postPageResponseMono
+                    = fetchTrendings(remainingCount - resultCount, regionCode, maxResultCount, trendingListResponse.getNextPageToken());
 
-            return Mono.just(trendingResponses).zipWith(postPageResponseMono, Stream::concat);
+            return Mono.just(trendingResponses).zipWith(postPageResponseMono,
+                    (l1, l2) -> Stream.concat(l1.stream(), l2.stream()).toList());
         });
     }
 
-    private static Stream<TrendingItem> mapFromResponsesToItems(LocalDateTime dateTime, String regionCode, Stream<VideoResponse> videoResponses) {
+    private static List<TrendingItem> mapFromResponsesToItems(LocalDateTime dateTime, String regionCode, List<VideoResponse> videoResponses) {
         Iterator<Integer> indexCounter = IntStream.iterate(0, i -> i + 1).iterator();
-        return videoResponses
+        return videoResponses.stream()
                 .map(videoResponse -> {
                     int rank = indexCounter.next() + 1;
                     String videoYoutubeId = videoResponse.getId();
                     return new TrendingItem(dateTime, regionCode, rank, videoYoutubeId);
-                });
+                })
+                .toList();
     }
 
 
