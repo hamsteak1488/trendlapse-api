28253c485ca20ad95c580085da6495301619f3fd
신재민 <woals1488@naver.com>
2026-01-10 00:15:52 +0900

    feat: 회원 정보 수정, 회원 탈퇴 API 엔드포인트를 추가

--- src/main/java/io/github/hamsteak/trendlapse/member/web/MemberController.java
+++ src/main/java/io/github/hamsteak/trendlapse/member/web/MemberController.java
@@ -1,14 +1,14 @@
 package io.github.hamsteak.trendlapse.member.web;
 
 import io.github.hamsteak.trendlapse.member.application.RegisterMemberService;
+import io.github.hamsteak.trendlapse.member.application.UpdateMemberService;
+import io.github.hamsteak.trendlapse.member.application.WithdrawMemberService;
 import io.github.hamsteak.trendlapse.member.application.dto.RegisterMemberCommand;
+import io.github.hamsteak.trendlapse.member.application.dto.UpdateMemberCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -17,11 +17,27 @@ import java.net.URI;
 @RequiredArgsConstructor
 public class MemberController {
     private final RegisterMemberService registerMemberService;
+    private final UpdateMemberService updateMemberService;
+    private final WithdrawMemberService withdrawMemberService;
 
     @PostMapping
     public ResponseEntity<?> registerMember(@Validated @RequestBody RegisterMemberCommand command) {
         long memberId = registerMemberService.register(command);
 
         return ResponseEntity.created(URI.create("/members/" + memberId)).build();
+    }
+
+    @PatchMapping
+    public ResponseEntity<?> updateMember(LoginMember loginMember, @Validated @RequestBody UpdateMemberCommand command) {
+        updateMemberService.update(loginMember.getMemberId(), command);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> withdrawMember(LoginMember loginMember) {
+        withdrawMemberService.withdraw(loginMember.getMemberId());
+
+        return ResponseEntity.ok().build();
     }
 }
