08d9569c2b5b8e298eb23bcfd9d23f649108a4c1
신재민 <woals1488@naver.com>
2026-02-14 00:05:19 +0900

    fix: 스프링을 일부 쓰는 테스트 환경에서 불필요하게 EnableJpaAuditing 애노테이션이 붙어 오류가 발생하는 문제를 해결하기 위해 애노테이션의 위치를 변경

--- src/main/java/io/github/hamsteak/trendlapse/TrendlapseApplication.java
+++ src/main/java/io/github/hamsteak/trendlapse/TrendlapseApplication.java
@@ -3,14 +3,12 @@ package io.github.hamsteak.trendlapse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
-@EnableJpaAuditing
 @EnableScheduling
 @EnableRetry(proxyTargetClass = true)
 @ConfigurationPropertiesScan
