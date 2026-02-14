b4a899366d79c96e703e8cc6dde885309c76a2c4
신재민 <woals1488@naver.com>
2026-02-14 15:16:48 +0900

    feat: AI 보고서 생성 엔드포인드 추가

--- src/main/java/io/github/hamsteak/trendlapse/report/snapshot/web/TrendingVideoRankingSnapshotReportController.java
+++ src/main/java/io/github/hamsteak/trendlapse/report/snapshot/web/TrendingVideoRankingSnapshotReportController.java
@@ -1,24 +1,30 @@
 package io.github.hamsteak.trendlapse.report.snapshot.web;
 
+import io.github.hamsteak.trendlapse.report.snapshot.application.CreateTrendingVideoRankingSnapshotReportService;
 import io.github.hamsteak.trendlapse.report.snapshot.application.GetTrendingVideoRankingSnapshotReportService;
 import io.github.hamsteak.trendlapse.report.snapshot.application.dto.TrendingVideoRankingSnapshotReportView;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/trendings/videos/ranking-snapshots")
 @RequiredArgsConstructor
 public class TrendingVideoRankingSnapshotReportController {
     private final GetTrendingVideoRankingSnapshotReportService getTrendingVideoRankingSnapshotReportService;
+    private final CreateTrendingVideoRankingSnapshotReportService createTrendingVideoRankingSnapshotReportService;
 
     @GetMapping("/{snapshotId}/report")
     public ResponseEntity<?> getSnapshotReport(@PathVariable long snapshotId) {
         TrendingVideoRankingSnapshotReportView view = getTrendingVideoRankingSnapshotReportService.get(snapshotId);
 
         return ResponseEntity.ok(view);
+    }
+
+    @PostMapping("/{snapshotId}/report")
+    public ResponseEntity<?> createSnapshotReport(@PathVariable long snapshotId) {
+        createTrendingVideoRankingSnapshotReportService.create(snapshotId);
+
+        return ResponseEntity.ok().build();
     }
 }
