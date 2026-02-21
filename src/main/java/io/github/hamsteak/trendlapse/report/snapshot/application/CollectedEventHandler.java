a9ff6bcf73ed2c9e76278629246c026b9b8ee9cf
신재민 <woals1488@naver.com>
2026-02-22 02:28:21 +0900

    chore: 보고서 생성 전 로깅 중복 제거

--- src/main/java/io/github/hamsteak/trendlapse/report/snapshot/application/CollectedEventHandler.java
+++ src/main/java/io/github/hamsteak/trendlapse/report/snapshot/application/CollectedEventHandler.java
@@ -7,8 +7,6 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 
-import java.util.List;
-
 @Component
 @Slf4j
 @RequiredArgsConstructor
@@ -18,14 +16,8 @@ public class CollectedEventHandler {
     @Async
     @TransactionalEventListener
     public void handle(CollectedEvent event) {
-        List<Long> snapshotIds = event.getCollectedSnapshotIds();
-        String snapshotIdsString = String.join(",", snapshotIds.stream().map(Object::toString).toList());
-        log.info("Starting to create report for snapshot ids {}", snapshotIdsString);
-
         event.getCollectedSnapshotIds().forEach(snapshotId ->
                 createTrendingVideoRankingSnapshotReportService.create(snapshotId)
         );
-
-        log.info("Completed to create report for snapshot ids {}", snapshotIdsString);
     }
 }
